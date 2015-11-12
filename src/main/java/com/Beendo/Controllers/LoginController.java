b9f60eb33d4ab84c592e2c66177162f4472ea0e3
hassanrazakhalid <hassanrazakhalid@yahoo.com>
2015-11-12 23:01:01 +0500

    Dashboard.xhtml added

--- src/main/java/com/Beendo/Controllers/LoginController.java
+++ src/main/java/com/Beendo/Controllers/LoginController.java
@@ -1,13 +1,29 @@
 package com.Beendo.Controllers;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
-@Component
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Controller
 public class LoginController {
 
+	private String userName;
+	private String password;
 	
 	public String getName(){
 		
 		return "Hello world123";
+	}
+	
+	public String loginPressed(){
+		
+		
+		return "Dashboard";
+		//System.out.println("InDell");
+		
 	}
 }
