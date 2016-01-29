9d52044907b3602b74657f09e36c0dec050615ac
hassanrazakhalid <hassanrazakhalid@yahoo.com>
2016-01-29 22:47:47 +0500

    ...

--- src/main/java/com/Beendo/Dao/ProviderDao.java
+++ src/main/java/com/Beendo/Dao/ProviderDao.java
@@ -120,7 +120,7 @@ public class ProviderDao implements IProvider {
 		// TODO Auto-generated method stub
 		Session session = this.sessionFactory.getCurrentSession();
 		
-		Query quey = session.createQuery("SELECT P FROM Provider P"
+		Query quey = session.createQuery("SELECT DISTINCT P FROM Provider P"
 				+ " JOIN P.practiceList Pra"
 				+ " JOIN Pra.entity E"
 				+ " WHERE E.id =:id");
